c54528ea0c9d8c5b476d9cfa284067ad6e8c6c9a
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-03-07 20:30:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
+const ShowSquare = ({value}) => {
+    return (<p id='output' value={value}>{Math.pow(value,2)}</p>)
+    // return "hello";
+  }
 const App = () => {
  //code here
 const [value, setValue] = React.useState(0);
@@ -8,8 +12,7 @@ const [value, setValue] = React.useState(0);
     <div id="main">
       <ChildComponent /> 
        <input id='input' onChange={(e) => {setValue(e.target.value)}}/>
-    
-      <p id='output' value={value}>{Math.pow(value,2)}</p>
+      <ShowSquare value = {value}/>
     </div>
   )
 }
